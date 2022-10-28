51282e69cdd008d5de4be980b33cc6fa1c8fbe5b
ThomassMathisen <thomasmathisen@hotmail.com>
2022-10-28 15:14:46 +0200

    Setup models

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  admin: { type: Boolean, required: true },
+})
+
+UserSchema.virtual('url').get(function(){
+  return `/catalog/user/${this._id}`
+})
+
+UserSchema.pre('save', function(){
+  const user = this
+
+  if (!user.isModified('password')){
+    return next()
+  }
+
+  bcrypt.hash(user.password, 10, function(err, hash){
+    if (err) {
+        return next(err)
+    }
+
+    user.password = hash
+    next()
+  })
+})
+
+module.exports = mongoose.model('User', UserSchema)
